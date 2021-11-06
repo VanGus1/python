0e3d220f8a58299f48d89832a66299117d4d0481
VanGus <gus1698@bk.ru>
2021-11-07 01:18:00 +0300

    Первое задание

---
+++ 1_2/1.py
@@ -0,0 +1,4 @@
+print("15 * 3 = " + str(15*3) + " " + str(type(15*3)))
+print("15 / 3 = " + str(15/3) + " " + str(type(15/3)))
+print("15 // 2 = " + str(15//2) + " " + str(type(15//2)))
+print("15 ** 2 = " + str(15**2) + " " + str(type(15**2)))
